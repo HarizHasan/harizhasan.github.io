52be4ddd3a7e065b936dba3048ea84f5e422084a
HarizHasan <redridingpilot@gmail.com>
2025-01-03 10:13:14 +0800

    Damn brackets

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ document.getElementById("sendBtn").addEventListener("click", async () => {
     const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
     const AUTH = "ya29.a0ARW5m76AKO1sxepG7MIdfOYvR9RHaKPB8y2nV5z-nzzxNGrMx4L_G45wwsqWb0zuJvFvfv_jMvnmfvxkzULk2vL1pQWa03Ga7O_ei_CXLr_uEo7xb9cAqWhkh2leDX5BCfXR2Fu4WpNXRSz1s1KKhaORsZMKVD7e3PXfzyYmaCgYKAZASARESFQHGX2MiDLXmpAWNvpQyaomsip2C-g0175"
 
+
     if (!userPrompt.trim()) {
         alert("Please enter a prompt!");
         return;
     }
+});
 
     // Clear the textarea
     document.getElementById("sendBtn").addEventListener("click", async () => {
